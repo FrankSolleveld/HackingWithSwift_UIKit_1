e6c75fb9f26046e617380e228e13ef71f72c1fb3
Frank Solleveld <mrmadman1300@gmail.com>
2020-12-09 10:32:43 +0100

    new biview + image view

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Frank Solleveld on 09/12/2020.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

5c71edc25ede2c8bd497d8f96950624da2a3fce2
Frank Solleveld <mrmadman1300@gmail.com>
2020-12-09 10:03:53 +0100

    added tableviewcontroller

---
+++ Project1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Frank Solleveld on 09/12/2020.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+
+}
+

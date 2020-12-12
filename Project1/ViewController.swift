04990b2574e2098fcddce28a5d5d069153d70ee9
FrankSolleveld <fsolleveld@outlook.com>
2020-12-12 10:27:05 +0100

    Resolved challenge 2, modified it a bit so my website is shared instead.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    // MARK: -- Custom Variables
     var pictures = [String]()
 
+    // MARK: -- Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTheApp))
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -28,6 +31,17 @@ class ViewController: UITableViewController {
         pictures = pictures.sorted(by: {$0 < $1})
     }
     
+    // MARK: -- Custom Methods
+    @objc func shareTheApp() {
+        let text: String? = "Visit my website!"
+        let url: URL? = URL(string: "https://devfrank.org")
+        guard let safeText = text, let safeUrl = url else { return }
+        let activityView = UIActivityViewController(activityItems: [safeText, safeUrl], applicationActivities: [])
+        activityView.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(activityView, animated: true)
+    }
+    
+    // MARK: -- Delegate Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
